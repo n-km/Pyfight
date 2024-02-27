31e9ff3c183959bf86d76a1e823c98de25eefd33
n-km <129538893+n-km@users.noreply.github.com>
2024-02-27 15:32:44 +0100

    0.2

---
+++ 0.2.py
@@ -0,0 +1,13 @@
+#Bitte hier den Namen ändern
+name = ("user")
+#Bitte hier das Geschlecht ändern
+#(Entweder 'Junge' oder 'Mädchen')
+geschlecht = ("###")
+
+#Jetzt können Sie das Programm schließen
+
+
+
+import time
+import os
+clear = lambda: os.system('cls')
