a58bd110179aceafae6098ce10c10cff2e7f20a9
n-km <129538893+n-km@users.noreply.github.com>
2024-02-27 17:27:52 +0100

    visitenkarte

--- 0.2.py
+++ 0.2.py
@@ -12,6 +12,14 @@ import time
 import os
 clear = lambda: os.system('cls')
 print("Name:", name)
+visitenkarteName = ("Name:", name)
 print("Geschlecht:", geschlecht)
+visitenkarteGeschlecht = ("Geschlecht:", geschlecht)
 sure = input("Stimmt das so? (J/N)")
-    if sure == (""):
+if sure == ("J"):
+    visitenkarte = (visitenkarteName, visitenkarteGeschlecht)
+    print("Ok, deine Visitenkarte wurde erstellt")
+if sure == ("N"):
+    exit("Du bist nicht einverstanden")
+time.sleep(2)
+print("User aktiv:", visitenkarte)
